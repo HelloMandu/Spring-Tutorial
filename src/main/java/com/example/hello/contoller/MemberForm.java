295d8b05a8e79bbfd33252def5f926d44130b337
HelloMandu <tjdals6695@gmail.com>
2023-03-07 20:33:33 +0900

    member join form 작성

---
+++ src/main/java/com/example/hello/contoller/MemberForm.java
@@ -0,0 +1,13 @@
+package com.example.hello.contoller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
